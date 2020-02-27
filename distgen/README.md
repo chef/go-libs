decec135d1f2b8c8818d1e03d1ed79967bdb0ac5
Tyler Ball <tball@chef.io>
2020-02-27 15:05:22 -0800

    Fixing line wrap length
    Signed-off-by: Tyler Ball <tball@chef.io>

--- distgen/README.md
+++ distgen/README.md
@@ -46,11 +46,11 @@ package dist
 
 ### Using an Environment Variable
 
-Using an environmental variable gives you more flexibility with CI systems.  
-As we saw in the custom JSON file example, passing a URI to `go run` in the third argument 
-overrides the default JSON file for `distgen`.
-The `DIST_FILE` environment performs the same function by overloading the default JSON file.
-The usage inside of your dist files will not change, but you can pass ENV as a part of your `go generate` command.
+Using an environmental variable gives you more flexibility with CI systems. As we saw in
+the custom JSON file example, passing a URI to `go run` in the third argument overrides
+the default JSON file for `distgen`. The `DIST_FILE` environment performs the same
+function by overloading the default JSON file. The usage inside of your dist files will
+not change, but you can pass ENV as a part of your `go generate` command.
 
 Example:
 ```shell script
--- distgen/doc.go
+++ distgen/doc.go
@@ -58,11 +58,11 @@ global variables to generate. (See an example of a JSON file at
 
 Using an Environment Variable
 
-Using an environmental variable gives you more flexibility with CI systems. As we saw
-in the custom JSON file example, passing a URI to "go run" in the third argument
-overrides the default JSON file for distgen. The DIST_FILE environment performs the same
+Using an environmental variable gives you more flexibility with CI systems. As we saw in
+the custom JSON file example, passing a URI to `go run` in the third argument overrides
+the default JSON file for `distgen`. The `DIST_FILE` environment performs the same
 function by overloading the default JSON file. The usage inside of your dist files will
-not change, but you can pass ENV as a part of your "go generate" command.
+not change, but you can pass ENV as a part of your `go generate` command.
 
   DIST_FILE="https://raw.githubusercontent.com/chef/go-libs/master/distgen/tiny_glob_dist.json" go generate
 
